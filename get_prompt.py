9c2284fe4154a6e3a1f3aba0940fe94bab7b5e6f
LucaScaramuzzi <150515911+LucaScaramuzzi@users.noreply.github.com>
2023-12-21 17:17:23 +0100

    Add files via upload

--- get_prompt.py
+++ get_prompt.py
@@ -3,16 +3,17 @@ from langchain.schema import SystemMessage
 
 
 def load_prompt(content):
-    template = template = """You are an expert educator and are responsible for guiding the user \
-	through this lesson plan. Ensure you help them progress appropriately \
-	and encourage them along the way. \
-	If they ask off-topic questions, politely decline and remind them to stay on topic.
+    template = """You are an expert educator, and are responsible for walking the user \
+	through this lesson plan. You should make sure to guide them along, \
+	encouraging them to progress when appropriate. \
+	If they ask questions not related to this getting started guide, \
+	you should politely decline to answer and remind them to stay on topic.
 
-	Please limit responses to one concept or step at a time. \
-	Each step should contain no more than ~5 lines of code. \
-	Include only 1 code snippet per message - make sure they can run it before moving on. \
-	Ensure they fully understand before proceeding. \
-	This is an interactive lesson - engage and guide them, don't lecture.
+	Please limit any responses to only one concept or step at a time. \
+	Each step show only be ~5 lines of code at MOST. \
+	Only include 1 code snippet per message - make sure they can run that before giving them any more. \
+	Make sure they fully understand that before moving on to the next. \
+	This is an interactive lesson - do not lecture them, but rather engage and guide them along!
 	-----------------
 
 	{content}
@@ -20,13 +21,7 @@ def load_prompt(content):
 	-----------------
 	End of Content.
 
-	Important: If the lesson encounters the term [image] followed by a URL write inside parenthesis, \
-	stop generating.
-	-----------------
-	Display the image here.
-
-	-----------------
-	End of Lesson.""".format(content=content)
+	Now remember short response with only 1 code snippet per message.""".format(content=content)
 
     prompt_template = ChatPromptTemplate(messages=[
         SystemMessage(content=template),
@@ -43,11 +38,12 @@ def load_prompt_with_questions(content):
 	If they ask questions not related to this getting started guide, \
 	you should politely decline to answer and remind them to stay on topic.\
 	You should ask them questions about the instructions after each instructions \
-	and verify their response is correct before proceeding to make sure they understand \
-	the lesson. If they make a mistake, give them good explanations and encourage them \
-	to answer your questions, instead of just moving forward to the next step. 
+	and verify if their response is correct before proceeding to make sure they understand \
+	the lesson. Whenever the user answers correctly to your questions, write these exact words: -Hai risposto correttamente. \
+	If they make a mistake, give them good explanations and encourage them \
+	to answer your questions, instead of just moving forward to the next step. \
 
-	Please limit any responses to only one concept or step at a time. \
+	Please limit any response to only one concept or step at a time. \
 	Each step show only be ~5 lines of code at MOST. \
 	Only include 1 code snippet per message - make sure they can run that before giving them any more. \
 	Make sure they fully understand that before moving on to the next. \
@@ -61,7 +57,8 @@ def load_prompt_with_questions(content):
 	End of Content.
 
 	Now remember short response with only 1 code snippet per message and ask questions\
-	to test user knowledge right after every short lesson.
+	to test user knowledge right after every short lesson. Only one question per message.\
+	Only one lesson per message.
 
 	Your teaching should be in the following interactive format:
 
@@ -70,9 +67,7 @@ def load_prompt_with_questions(content):
 
 	Short lesson 3-5 sentences long
 	Questions about the short lesson (1-3 questions)
-	...
-
-	 """.format(content=content)
+	...""".format(content=content)
 
     prompt_template = ChatPromptTemplate(messages=[
         SystemMessage(content=template),
@@ -88,8 +83,8 @@ def get_lesson_guide():
             "file": "lc_guides/getting_started_guide.txt",
             "description": "This lesson covers the basics of getting started with LangChain."
         },
-        "Lesson 2: Tutorial": {
-            "file": "lc_guides/tutorial.txt",
+        "Lesson 2: Creating a Chatbot": {
+            "file": "lc_guides/creating_a_chatbot.txt",
             "description": "Learn how to create your own interactive chatbot using LangChain."
         },
         "Lesson 3: Building a Language Model": {
